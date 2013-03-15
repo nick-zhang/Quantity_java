457443e8152daf728eecd276b707beef71d3a7c9
Zhang Yue <zhangyue02135@yahoo.com.cn>
2013-03-15 12:13:02 +0800

    Added equals and hashcode

--- src/com/tw/oo/Quantity.java
+++ src/com/tw/oo/Quantity.java
@@ -8,6 +8,31 @@ package com.tw.oo;
  * To change this template use File | Settings | File Templates.
  */
 public class Quantity {
+    private int amount;
+    private Object unit;
+
     public Quantity(int amount, Object unit) {
+        this.amount = amount;
+        this.unit = unit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Quantity quantity = (Quantity) o;
+
+        if (amount != quantity.amount) return false;
+        if (!unit.equals(quantity.unit)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = amount;
+        result = 31 * result + unit.hashCode();
+        return result;
     }
 }
