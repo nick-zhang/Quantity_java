4b587b15b001be1340b07e608e2d10ab2c13a4ad
Zhang Yue <zhangyue02135@yahoo.com.cn>
2013-03-15 10:20:12 +0800

    init commit

---
+++ test/com/tw/oo/LengthTest.java
@@ -0,0 +1,17 @@
+package com.tw.oo;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: NickZhang
+ * Date: 3/15/13
+ * Time: 9:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LengthTest {
+    @Test
+    public void should_test_length(){
+
+    }
+}
